ca8c363234f3d5c1d95fa966b9824deadbbb646c
Igor Drakulic <rogicilukard@gmail.com>
2026-05-20 16:51:40 +0200

    last commit of the day

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-let age = 25;
+console.log("Hello, World!");
 
-console.log(age);
+let developer = "Jessica";
+console.log(developer);
+developer = "Quincy";
+console.log(developer);
+
+
+let firstName = "Igor";
+let lastNmae = "Stravinshki";
+let fullName = firstName + " " + lastNmae;
+console.log(fullName);
+
+
+
+let somethingSpecial = "Today is 2nd day of Cdemy";
+somethingSpecial += " and i am working on my JS skills.";
+console.log(somethingSpecial);
+
+/* string concatenation */
+let str1 = "Hello";
+let str2 = "World.";
+let str12 = str1 + " " + str2; 
+
+console.log("This is string concatenation:");
+console.log(str12);
+
+console.log("Here will concat() method be used : ");
+let result = str1.concat(" " + str2);
+console.log(result);   
+
+
+let variabla = 10;
+console.log(variabla);
+
+
+// comment 
+
+/* multi
+ line
+  comment
+*/
+
+const name = "Alice";
+const age = 25;
+const message = `My name is ${name} and I am ${age} years old.`;
+console.log(message);
+
+
+
+let sentence = "JavaScript is awesome, and JavaScript is powerful!";
+let position = sentence.indexOf("JavaScript", 20);
+console.log(position);
